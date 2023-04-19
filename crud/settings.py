ac339c5dd701a3b10b9c019b78f48f118d2f97f1
sungshin52 <sungshin52@naver.com>
2023-04-19 14:59:20 +0900

    add allowed hosts

--- crud/settings.py
+++ crud/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://port-0-sample-project-17xqnr2algna5g63.sel3.cloudtype.app/']
 
 
 # Application definition
